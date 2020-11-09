091684c79f45531220f2e6f07014038f9008c57b
Colin-Yun <doit_again@naver.com>
2020-11-09 14:59:57 +0900

    django test code be commited

--- myfirst_site/myfirst_site/urls.py
+++ myfirst_site/myfirst_site/urls.py
@@ -13,9 +13,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls import url, include
 from django.contrib import admin
-from django.urls import path
+# from django.conf.urls import include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    url('admin/', admin.site.urls), #app 접속을 위해 include를 씁니다.
+    url('', include('elections.urls')), #localhost:8000으로 요청이 들어오면 elections.urls로 전달
 ]
